ba03fc44ac8cbc965774e39078f5e012864c1678
nitin272 <142768664+nitin272@users.noreply.github.com>
2023-11-21 16:59:39 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css';
+import lake from "./image/lake.JPG";
+import GallaryFooter from './components/GallaryFooter';
+import Header from './components/Header';
+import Body from './components/Body';
+const imageData = [
+  {
+    id:1,
+    img:lake 
+  },
+  {
+    id:2,
+    img:lake
+  },
+  {
+    id:3,
+    img:lake
+  },
+  {
+    id:4,
+    img:lake
+  }
+]
+
+function App() {
+  return (
+    <>
+    <div>
+      <Header/>
+      <Body imageData = {imageData}/>
+      {/* adding footer component */}
+      <GallaryFooter/> 
+    </div>
+    </>
+  )
+}
+
+export default App;
